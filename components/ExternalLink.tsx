84829b5803f89645899a6ba6485b020d5affc442
nihalsaeed <nihalsaeed@gmail.com>
2026-05-15 03:03:29 +0530

    web and ios removed

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'expo-router';
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import { Platform } from 'react-native';
 
 export function ExternalLink({
   href,
@@ -12,14 +10,6 @@ export function ExternalLink({
       target="_blank"
       {...props}
       href={href}
-      onPress={(e) => {
-        if (Platform.OS !== 'web') {
-          // Prevent the default behavior of linking to the default browser on native.
-          e.preventDefault();
-          // Open the link in an in-app browser.
-          WebBrowser.openBrowserAsync(href as string);
-        }
-      }}
     />
   );
 }
